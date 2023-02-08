562e305e3ef532401b17a040df8241a8175bb03b
Vaibhav Mahajan <i.vaibhavmahajan@gmail.com>
2023-02-08 15:24:26 -0500

    chore(product): Added put api for product

--- product/views.py
+++ product/views.py
@@ -63,7 +63,7 @@ class ProductGetView(generics.RetrieveUpdateDestroyAPIView):
     requires the user to be authenticated.
     Uses ProductUpdateSerializer for updating a Product.
     """
-    http_method_names = ['get', 'patch', 'delete']
+    http_method_names = ['get', 'patch', 'delete', 'put']
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = ProductUpdateSerializer
@@ -72,7 +72,7 @@ class ProductGetView(generics.RetrieveUpdateDestroyAPIView):
         """
         Returns permission classes based on the request method.
         """
-        if self.request.method in ['PATCH', 'DELETE']:
+        if self.request.method in ['PATCH', 'DELETE', 'PUT']:
             return [permission() for permission in self.permission_classes]
         else:
             return []
@@ -139,6 +139,59 @@ class ProductGetView(generics.RetrieveUpdateDestroyAPIView):
 
             # Get the serializer with the current product instance and request data
             serializer = self.get_serializer(instance=product, data=request.data, partial=True)
+
+            # Validate the serializer data
+            if serializer.is_valid():
+                # Save the changes in the database using transaction
+                with transaction.atomic():
+                    serializer.save()
+                # Retrieve the updated product from the database
+                product = Product.objects.filter(sku=serializer.data.get('sku')).values().first()
+                return response(True, "Product data updated successfully", status.HTTP_200_OK, data=product)
+            else:
+                return response(False, serializer.errors, status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            # Return the error message with a timeout status code in case of any unexpected error
+            return response(False, str(e), status.HTTP_408_REQUEST_TIMEOUT)
+
+    def put(self, request, *args, **kwargs):
+        """
+        Handle PUT request to update a specific Product.
+        Returns a response with success or failure message and relevant HTTP status code.
+
+        :param request: The incoming request
+        :param args: Additional positional arguments
+        :param kwargs: Additional keyword arguments, including the `id` of the Product to update
+        :return: A response object with success or failure message and relevant HTTP status code
+        """
+        try:
+            # Check if the requested Product exists in the database
+            if not Product.objects.filter(id=kwargs['id']).exists():
+                return response(False, "Product does not exist", status.HTTP_404_NOT_FOUND)
+
+            if request.data.get("quantity", None) and type(request.data.get("quantity")) is str:
+                return response(False, "quantity field cannot be of type string", status.HTTP_400_BAD_REQUEST)
+
+            # Retrieve the Product object from the database
+            product = Product.objects.get(id=kwargs['id'])
+
+            # Check if the requesting user is the owner of the Product
+            if not product.owner_user == request.user:
+                return response(False, "You are not allowed to change this product's data", status.HTTP_403_FORBIDDEN)
+
+            # Check if the request data is empty
+            if not request.data:
+                return response(False, "No data to update", status.HTTP_204_NO_CONTENT)
+
+            # Check if the SKU of the Product exists in the request data and it is different from the current SKU
+            # and a Product with the new SKU already exists in the database
+            if request.data.get("sku", False) and \
+                request.data['sku'] != product.sku and \
+                Product.objects.filter(sku=request.data['sku']).exists():
+                return response(False, "Product with this SKU already exists", status.HTTP_400_BAD_REQUEST)
+
+            # Get the serializer with the current product instance and request data
+            serializer = self.get_serializer(instance=product, data=request.data)
 
             # Validate the serializer data
             if serializer.is_valid():
